33cad33d59065c379f3f321392cf03b0072747c0
Alejandro Agra <aleagra08@hotmail.com>
2025-11-27 15:15:15 -0300

    first commit

---
+++ src/main/java/com/example/therapify/TherapifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.therapify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherapifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TherapifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/therapify/TherapifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.therapify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TherapifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
