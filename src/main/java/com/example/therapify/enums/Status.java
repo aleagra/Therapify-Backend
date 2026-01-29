f1fcc21a95d99fd050b751e966747b5e0fa6837b
Alejandro Agra <aleagra08@hotmail.com>
2026-01-29 14:12:19 -0300

    enums

---
+++ src/main/java/com/example/therapify/enums/Status.java
@@ -0,0 +1,7 @@
+package com.example.therapify.enums;
+
+public enum Status {
+    PENDING,
+    CONFIRMED,
+    COMPLETED
+}
---
+++ src/main/java/com/example/therapify/enums/UserType.java
@@ -0,0 +1,5 @@
+package com.example.therapify.enums;
+
+public enum UserType {
+    PACIENTE,DOCTOR,ADMIN
+}
