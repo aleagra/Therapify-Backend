d1b87b6811e2556952dd447b3d530778967d6af9
Alejandro Agra <aleagra08@hotmail.com>
2026-03-31 15:23:23 -0300

    security

--- src/main/java/com/example/therapify/config/SecurityConfig.java
+++ src/main/java/com/example/therapify/config/SecurityConfig.java
@@ -36,7 +36,7 @@
         public CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration config = new CorsConfiguration();
 
-            config.setAllowedOrigins(List.of("http://localhost:4200"));
+            config.setAllowedOrigins(List.of("http://localhost:4200", "https://therapifyy.vercel.app"));
             config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS","PATCH"));
             config.setAllowedHeaders(List.of("*"));
             config.setAllowCredentials(true);
